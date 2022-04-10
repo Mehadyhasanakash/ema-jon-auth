24f7784b9f1fd1ec3b424a08f4dafc3b611142cb
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-04-10 21:37:52 +0600

    login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className='from-container'>
+           <div>
+           <h1 className='from-title'> Login</h1>
+            <form >
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="" placeholder='Please Your Email'/>
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="" placeholder='password' />
+            </div>
+            <input className='form-submit' type="submit" value="login" />
+            </form>
+            <p>New to Ema-John? <Link className='form-link' to='/signup'>Create Account</Link></p>
+           </div>
+        </div>
+    );
+};
+
+export default Login;
